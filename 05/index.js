0769d72b2bd9b57bece9708942bb34095af4af19
fasosnql <nowak_przemek@yahoo.com>
2021-02-20 14:53:33 +0100

    05-finish

--- 05/index.js
+++ 05/index.js
@@ -1,17 +1,22 @@
 import { GraphQLServer } from 'graphql-yoga'
 import {RootBookResolver, Book} from './src/resolvers/BookResolvers'
+import {RootAuthorResolver} from './src/resolvers/AuthorResolvers'
+import {RootCommentResolver} from './src/resolvers/CommentResolvers'
 import db from './db';
 
 const typeDefs = `
   type Query {
     getBooks: [Book!]!
     getAuthor(id: ID!): Author
+    getBook(id: ID!): Book
+    getComments: [Comment!]!
   }
   
   type Book {
       id: ID!
       title: String!
       authors: [Author!]!
+      comments: [Comment!]!
   }
   
   type Author {
@@ -19,11 +24,20 @@ const typeDefs = `
       firstName: String!
       lastName: String!
   }
+  
+  type Comment {
+      id: ID!
+      content: String!
+      bookId: ID!
+      approved: Boolean
+  }
 `
 
 const resolvers = {
   Query: {
-    ...RootBookResolver
+    ...RootBookResolver,
+    ...RootAuthorResolver,
+    ...RootCommentResolver
   },
   Book,
 }
--- 05/src/resolvers/BookResolvers.js
+++ 05/src/resolvers/BookResolvers.js
@@ -1,11 +1,17 @@
 export const RootBookResolver = {
     getBooks: (_, args, {db}) => {
         return db.books;
+    },
+    getBook: (_, {id}, {db}) => {
+        return db.books.find(book => book.id === id)
     }
 }
 
 export const Book = {
     authors: ({authors}, args, {db}) => {
         return db.authors.filter(author => authors.includes(author.id))
+    },
+    comments: ({id}, args, {db}) => {
+        return db.comments.filter(comment => comment.bookId === id)
     }
 }
